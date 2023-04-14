213cab0ab53a86f556a44a10b1147fa457caeae0
leeyoonseo <diano679@gmail.com>
2023-04-14 20:21:26 +0900

    230414 END

--- 2. web-api/10. 종합/blackJack/app.js
+++ 2. web-api/10. 종합/blackJack/app.js
@@ -7,9 +7,13 @@ $gameOn.style.display = 'none';
 
 $startBtn.addEventListener('click', e => {
     $start.classList.toggle('hide');
-    $start.style.transition = '1s';
-    $gameOn.style.transition = '1s';
     $gameOn.style.display = 'flex';
+    $gameOn.style.opacity = '0';
+    setTimeout(e =>{
+        $start.style.transition = '1s';
+        $gameOn.style.transition = '0.5s';
+        $gameOn.style.opacity = '1';
+    }, 400)
 })
 
 
@@ -26,12 +30,16 @@ const gameData = {
 
 const $playerUl = document.getElementById('player-card');
 const $dealerUl = document.getElementById('dealer-card');
-let standFlag = false;
+let standFlag = false; //내 카드인지 딜러의 카드인지 여부
 // 카드 뽑기 함수 (40개 중 랜덤) 
 let cardNum = 0;
-let poCard = 0; // 카드 모양 나타내기 위해 만든 변수
+let rCard = [];
 function randomCard() {
-    let rCard = [];
+    if(rCard.length>=38){ //카드를 다 쓰면 셔플하기.
+        console.log('카드를 다시 셔플합니다.')
+        rCard.splice(0);
+    }
+
     let random = Math.floor(Math.random() * 40) + 1;
     while (rCard.includes(random)) {
         random = Math.floor(Math.random() * 40) + 1;
@@ -40,16 +48,12 @@ function randomCard() {
     rCard.push(random);
     cardNum = random;
 
-    if (random >= 1 && random <= 10) {
-        poCard = '♠' + random;
-    } else if (random >= 11 && random <= 20) {
-        poCard = '♣' + (random - 10);
+    if (random <= 10) { //결과에 따라 - 진행
+    } else if (random <= 20) {
         cardNum -= 10;
-    } else if (random >= 21 && random <= 30) {
-        poCard = '♥' + (random - 20);
+    } else if (random <= 30) {
         cardNum -= 20;
-    } else if (random >= 31 && random <= 40) {
-        poCard = '⧫' + (random - 30);
+    } else if (random <= 40) {
         cardNum -= 30;
     };
 
@@ -71,15 +75,19 @@ let $betMoney = document.getElementById('betting-text-money');
 let $poMoney = document.getElementById('pocket-text-money');
 
 // 배팅 실시간 설정
+let bett = 0;
 $betting.addEventListener('click', e => {
     if (!e.target.matches('input')) return;
-    if (e.target.value > gameData.myMoney) return;
+    if (e.target.value === 'All-in'){ //헤헤
+        console.log('올인선택');
+        bett = gameData.myMoney;
+    } else{
+        bett = e.target.value;
+    }
+    if (bett > gameData.myMoney) return;
 
-    gameData.myBetting += (+e.target.value);
-    gameData.myMoney -= (+e.target.value);
-    console.log('배팅금액: '+gameData.myBetting); //확인용
-    console.log('내 돈: '+gameData.myMoney); //확인용
-
+    gameData.myBetting += (+bett);
+    gameData.myMoney -= (+bett);
     $betMoney.textContent = gameData.myBetting;
     $poMoney.textContent = gameData.myMoney;
 })
@@ -138,7 +146,7 @@ $hitBtn.addEventListener('click', e => {
     } else if (gameData.myCard > 21) {
         return;
     }
-
+    $addScore.style.color = 'rgb(255, 187, 0)';
     let hitCard = randomCard();
     gameData.myCard += (+hitCard); //내 카드의 총합 더하기
     $myPoint.textContent = gameData.myCard;
@@ -146,39 +154,54 @@ $hitBtn.addEventListener('click', e => {
     if (gameData.myCard > 21) {
         setTimeout(e=>{
             alert('딜러 승리!')
+            roundLose()
             loseCheck();
-            roundReset()
+            roundReset();
             return;
-        }, 900)
+        }, 800)
     }
 })
 
 
 // stand를 눌러서 딜러의 차례가 시작되는 함수
 const $endgame = document.querySelector('#end')
+const $gameScoreIng = document.getElementById('gameOn-score');
+const $addMoney = document.getElementById('addMoney');
+const $addMoneyEm = document.querySelector('#addMoney>em');
+const $addScore = document.getElementById('addScore');
+const $addScoreEm = document.querySelector('#addScore>em');
 
 $standBtn.addEventListener('click', e => {
-    if (gameData.myCard === 0) { //카드를 안뽑고 멈추면 안됨.
-        return;
+    if (gameData.myCard === 0) { return; //카드를 안뽑고 멈추면 안됨.
     } else if(gameData.myCard > 21) return;
+    if(standFlag) return;
     
     standFlag = true;
     let func = setInterval(() => {
 
         let standCard = randomCard();
         gameData.dealCard += (+standCard);
-        console.log(standCard);
 
         $youPoint.textContent = gameData.dealCard
         if (gameData.dealCard >= 17) {
             setTimeout(e=>{
                 if (gameData.dealCard > 21 || gameData.dealCard < gameData.myCard) {
-                    alert('플레이어 승리!')
                     $poMoney.textContent = gameData.myMoney + gameData.myBetting * 2;
                     gameData.myMoney += gameData.myBetting * 2;
-    
+                    
+                    gameData.myScore += gameData.myBetting*10;
+                    $gameScoreIng.textContent = gameData.myScore;
+                    $addScoreEm.textContent = '+ '+gameData.myBetting*10;
+                    $addScore.classList.add('win') //스코어 올라감
+
+                    $addMoneyEm.textContent = gameData.myBetting*2;
+                    $addMoney.classList.add('win') //소지금 올라감
+
+                    alert('플레이어 승리!')
+                    
                 } else if (gameData.dealCard > gameData.myCard) {
                     alert('딜러 승리!')
+                    roundLose()
     
                 } else if (gameData.dealCard === gameData.myCard) {
                     alert('비겼음')
@@ -186,26 +209,45 @@ $standBtn.addEventListener('click', e => {
                     $poMoney.textContent = gameData.myMoney;
                 }
                 clearInterval(func);
-                roundReset();
                 loseCheck();
+                roundReset();
                 return;
-            }, 900)
+            }, 800)
         }
-    }, 1000);
+    }, 900);
 
 })
 
-const $reStart = document.getElementById('end-btn');
 
+const $reStart = document.getElementById('end-btn');
+const $gameScoreEnd = document.getElementById('last-score');
 
 //지는 조건을 체크
 function loseCheck(){
     if (gameData.myMoney <= 0) {
-        $gameOn.style.transition = '1.5s'
+        $gameScoreEnd.textContent = gameData.myScore;
+        $gameOn.style.transition = '1.1s'
         $gameOn.style.opacity = '0.2';
         $gameOn.style.backgroundColor = '#000000';
         $endgame.classList.toggle('hide');
+    } else{
+        $shuffle.style.display = 'block';
+        $shuffle.style.opacity = '0';
+        $shuffle.style.transition = '0.2s';
+        setTimeout(e=>{
+            $shuffle.style.opacity = '1';
+        }, 100)
+        $betting.classList.remove('hide');
     }
+}
+//딜러 승리시
+function roundLose() {
+    if(gameData.myScore<=0)return;
+    $addScore.style.color = 'red';
+    $addScoreEm.textContent = '- '+(gameData.myBetting*10);
+    $addScore.classList.add('win') //스코어 내려감
+    gameData.myScore -= gameData.myBetting*10;
+    $gameScoreIng.textContent = gameData.myScore;
 }
 
 //라운드 종료시 초기화하는 함수
@@ -217,14 +259,10 @@ function roundReset() {
     gameData.myCard = 0;
     $myPoint.textContent = 0;
     $youPoint.textContent = 0;
-    setTimeout(e=>{
-        $betting.style.display = 'block';
-        $shuffle.style.display = 'block';
-    }, 400)
     $hitStand.classList.toggle = ('on');
     flag = false;
     standFlag = false;
-
+    
     while($playerUl.firstChild){
         $playerUl.removeChild($playerUl.firstChild);
     }
@@ -232,9 +270,10 @@ function roundReset() {
         // $dealerUl.firstChild.style.animation = 'dealer-card-down 0.5s cubic-bezier(0.33, 1, 0.68, 1) 0.2s both';
         $dealerUl.removeChild($dealerUl.firstChild);
     }
-
-    $betting.classList.remove('hide');
-    
+    setTimeout(e=>{
+        $addMoney.classList.remove('win')
+        $addScore.classList.remove('win')
+    }, 820)
 
 }
 
